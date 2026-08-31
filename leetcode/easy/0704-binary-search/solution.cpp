d16dd8026d5a2df38e1a9cdf244548fe47e4218a
Rohit Singh <rohitsinghudi1372008@gmail.com>
2026-08-31 23:17:12 +0530

    ✅ Binary Search | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 31.5 MB | 2026-08-31

---
+++ leetcode/easy/0704-binary-search/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int begin=0; int end=nums.size()-1;
+        while(begin<=end){
+            int mid=(begin+end)/2;
+             if (nums[mid] == target) {
+                return mid;
+            }
+            else if (nums[mid] < target) {
+                begin = mid + 1;
+            }
+            else {
+                end = mid - 1;
+            }
+        }
+
+        return -1;
+        }
+};
