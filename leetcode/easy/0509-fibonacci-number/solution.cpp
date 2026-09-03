6780c5b15c2bdee906277e4d9fd0d9ba5b80e04e
Rohit Singh <rohitsinghudi1372008@gmail.com>
2026-09-03 11:09:02 +0530

    ✅ Fibonacci Number | Easy | C++ | Runtime: 3 ms | Memory: 7.8 MB | 2026-09-03

--- leetcode/easy/0509-fibonacci-number/solution.cpp
+++ leetcode/easy/0509-fibonacci-number/solution.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n<=1) return n;
-        int a=0;
-        int b=1;
-        for(int i=2; i<=n; i++){
-           int c=a+b;
-            a=b;
-            b=c;
+        if (n <= 1) return n;
+
+        vector<int> dp(n + 1);
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
-        return b;
+
+        return dp[n];
     }
 };
